c2018e05d20b9a4be3eec0133396ad15cb30bded
rob-bee-neilson <robbie.neilson@gmail.com>
2023-11-20 21:00:07 +1300

    add case statement and methods for card_type check

--- app/card_number_processor.rb
+++ app/card_number_processor.rb
@@ -5,7 +5,20 @@ class CardNumberProcessor
   end
   
   def card_type
-    return 'Unknown'
+    return 'Unknown' unless @card_number.start_with?('6014')
+
+    length = @card_number.length
+
+    case valid_card_number_format?(@card_number)
+    when black_card?(length)
+      return 'Flybuys Black'
+    when red_card?(length)
+      return 'Flybuys Red'
+    when green_card?(length)
+      return 'Flybuys Green'
+    else
+      return 'Flybuys Blue'
+    end
   end
   
   def valid?
@@ -21,6 +34,20 @@ class CardNumberProcessor
 
   def valid_card_number_format?(card_number)
     card_number.match?(/\A\d{16,17}\z/)
+  end
+
+  def black_card?(length)
+    (@card_number[4] == '1') && (length == 16 || length == 17)
+  end
+  
+  def red_card?(length)
+    @card_number.start_with?('6014352') && length == 16
+  end
+  
+  def green_card?(length)
+    @card_number.start_with?('601435552') &&
+      (6..9).include?(@card_number[9].to_i) &&
+      length == 16
   end
 end
   
